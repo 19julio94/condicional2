5bd3f368669e2fbc8214948197cf15e870c1deb4
jpatriciodasilva <jpatriciodasilva@atanasoff03.danielcastelao.org>
2015-11-16 09:08:10 +0100

    inicio

---
+++ src/exemplocondicional2/ExemploCondicional2.java
@@ -0,0 +1,24 @@
+
+
+package exemplocondicional2;
+
+/**
+ *
+ * @author jpatriciodasilva
+ */
+public class ExemploCondicional2 {
+
+  public static void main(String[] args) {
+    int numero1,numero2;
+    Compara obx= new Compara();
+    numero1= obx.pedirNumeroEnteiro();
+    numero2= obx.pedirNumeroEnteiro();
+    
+    obx.comparar(numero1, numero2);
+    
+    
+    
+    
+    }
+    
+}
